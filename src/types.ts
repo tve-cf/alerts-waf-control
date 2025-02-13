a512304e45aeff891441d27065ba03b9e6294dfc
Amirul Zharfan Bin Zalid <amirulzharfanbin@cloudflare.com>
2025-02-13 22:39:40 +0800

    Move types to types.ts

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,38 @@ export interface Settings {
   secret: string;
 }
 
+export interface CloudflareResponse<T> {
+  success: boolean;
+  result: T;
+  error: string | null;
+  errors?: Array<{
+    code: number;
+    message: string;
+  }>;
+  messages?: string[];
+  message?: string;
+}
+
+export interface Zone {
+  id: string;
+  name: string;
+}
+
+export interface WAFRuleResponse {
+  rulesetId: string;
+  rules: WAFRule[];
+}
+
+export interface WAFRule {
+  id?: string;
+  version?: string;
+  action?: string;
+  description?: string;
+  enabled?: boolean;
+  ref?: string;
+  last_updated?: string;
+}
+
 export interface Bindings {
   SETTINGS: KVNamespace;
 }
